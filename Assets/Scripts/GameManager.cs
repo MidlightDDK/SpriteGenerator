35683e4389ea65697297be19f6d42b70e22a4755
AZAR Majed <57988914+MidlightDDK@users.noreply.github.com>
2026-07-15 18:12:16 +0000

    Some cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,8 +23,6 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!");
-
         RunGenerationDemo();
     }
 
